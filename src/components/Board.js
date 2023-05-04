dab7356a55771383e7ee7fedce935017bc4c81fb
John Doe <nickhumphrey@outlook.com>
2023-05-04 10:20:14 +0100

    First commit

---
+++ src/components/Board.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Player from "./Player";
+
+function Board() {
+  const [player1LifeTotal, setPlayer1LifeTotal] = useState(20);
+  const [player2LifeTotal, setPlayer2LifeTotal] = useState(20);
+
+  const handlePlayer1Increment = () => {
+    setPlayer1LifeTotal(player1LifeTotal + 1);
+  };
+
+  const handlePlayer1Decrement = () => {
+    if (player1LifeTotal > 1) {
+      setPlayer1LifeTotal(player1LifeTotal - 1);
+    }
+  };
+
+  const handlePlayer2Increment = () => {
+    setPlayer2LifeTotal(player2LifeTotal + 1);
+  };
+
+  const handlePlayer2Decrement = () => {
+    if (player2LifeTotal > 1) {
+      setPlayer2LifeTotal(player2LifeTotal - 1);
+    }
+  };
+
+  return (
+    <div className="bg-gray-100 min-h-screen items-center">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold text-center mb-4">
+          Magic: The Gathering Life Counter
+        </h1>
+        <div className="flex flex-wrap">
+          <Player
+            id={1}
+            name="Player 1"
+            lifeTotal={player1LifeTotal}
+            handleIncrement={handlePlayer1Increment}
+            handleDecrement={handlePlayer1Decrement}
+            isFlipped={true}
+          />
+          <Player
+            id={2}
+            name="Player 2"
+            lifeTotal={player2LifeTotal}
+            handleIncrement={handlePlayer2Increment}
+            handleDecrement={handlePlayer2Decrement}
+            isFlipped={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/components/Player.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Player(props) {
+  const {
+    id,
+    name,
+    lifeTotal,
+    handleIncrement,
+    handleDecrement,
+    isFlipped,
+    background,
+  } = props;
+  const cardClassName = `bg-cyan-600 w-64 h-96 rounded-lg shadow-lg text-center p-4 ${
+    isFlipped ? "transform rotate-180" : ""
+  } bg-${background}-500`;
+
+  return (
+    <div className="flex justify-center pt-2 mt-12 w-full md:w-1/2 p-2">
+      <div className={cardClassName}>
+        <h2 className="text-white	 text-xl font-bold mb-4">{name}</h2>
+        <p className="text-white	 text-6xl font-bold mb-8">
+          {lifeTotal}
+        </p>
+        <div className="flex justify-around">
+          <button
+            className="text-white	text-7xl		"
+            onClick={handleDecrement}
+          >
+            -
+          </button>
+          <button
+            className="text-white	text-7xl	"
+            onClick={handleIncrement}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Player;
