bf9f8031655477729e2cebd70815b90ca786a39f
John Doe <nickhumphrey@outlook.com>
2023-05-04 12:52:17 +0100

    Added background colour changer

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import React from "react";
 
 function Player(props) {
+  const [backgroundColor, setBackgroundColor] = useState("#0891b2");
+
+  function handleBackgroundColorChange(newColor) {
+    setBackgroundColor(newColor);
+  }
+
   const {
     lifeTotal,
     handleIncrement,
@@ -14,9 +21,13 @@ function Player(props) {
 
   return (
     <div className="flex justify-center  w-full md:w-1/2 p-4">
-      <div className={cardClassName}>
-        <form className="m-6 flex justify-center items-center ">
+      <div className={cardClassName} style={{ backgroundColor }}>
+        <form
+          style={{ backgroundColor }}
+          className="m-6 flex justify-center items-center "
+        >
           <input
+            style={{ backgroundColor }}
             type="text"
             className="w-52 text-white placeholder-white bg-cyan-600 text-center  text-xl font-bold mb-4"
             placeholder="Enter player name"
@@ -37,6 +48,22 @@ function Player(props) {
             onClick={handleIncrement}
           >
             +
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <input
+            className="border-2 border-white border-solid"
+            type="color"
+            value={backgroundColor}
+            onChange={(e) =>
+              handleBackgroundColorChange(e.target.value)
+            }
+          />
+          <button
+            className="m-2 p-2 rounded-md  text-white "
+            onClick={() => handleBackgroundColorChange("#0891b2")}
+          >
+            Reset background
           </button>
         </div>
       </div>
